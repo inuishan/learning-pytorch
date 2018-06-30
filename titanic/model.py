003af707dcc9b0e553b26abaf1379e7b3a55cc67
Ishan Jain <ishanjain1991@gmail.com>
2018-06-30 22:36:40 +0530

    using label encoder

---
+++ titanic/model.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+dataset = pd.read_csv('train.csv')
+X_test = pd.read_csv('test.csv')
+
+# There is Mr and Mrs in name, lets extract it out.
+# Example 'Braund, Mr. Owen Harris'
+dataset_title = [i.split(',')[1].split('.')[0].strip() for i in dataset['Name']]
+dataset_title = pd.Series(dataset_title)
+print(dataset_title.value_counts(sort=True))
+
+dataset['Title'] = dataset_title.replace([['Lady', 'the Countess', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major',
+                                           'Rev', 'Sir', 'Jonkheer', 'Dona', 'Ms', 'Mme', 'Mlle'], 'Rare'])
+
+# Whether family was present or not
+dataset["FamilyS"] = dataset["SibSp"] + dataset['Parch'] + 1
+X_test["FamilyS"] = X_test["SibSp"] + X_test["Parch"] + 1
+
+
+def family(x):
+    if x < 2:
+        return 'Single'
+    elif x == 2:
+        return 'Couple'
+    elif x <= 4:
+        return 'InterM'
+    else:
+        return 'Large'
+
+
+dataset['FamilyS'] = dataset["FamilyS"].apply(family)
+X_test['FamilyS'] = X_test["FamilyS"].apply(family)
+
+dataset['Embarked'].fillna(dataset['Embarked'].mode()[0], inplace=True)
+
+X_test['Embarked'].fillna(X_test['Embarked'].mode()[0], inplace=True)
+dataset['Age'].fillna(dataset['Age'].median(), inplace=True)
+X_test['Age'].fillna(X_test['Age'].median(), inplace=True)
+X_test['Fare'].fillna(X_test['Fare'].median(), inplace=True)
+
+dataset = dataset.drop(['PassengerId', 'Cabin', 'Name', 'SibSp', 'Parch', 'Ticket'], axis=1)
+X_test_passengers = X_test['PassengerId']
+X_test = X_test.drop(['PassengerId', 'Cabin', 'Name', 'SibSp', 'Parch', 'Ticket'], axis=1)
+
+# Features
+X_train = dataset.iloc[:, 1:9].values
+
+# Output
+Y_train = dataset.iloc[:, 0].values
+
+labelencoder_X_1 = LabelEncoder()
+X_train[:, 1] = labelencoder_X_1.fit_transform(X_train[:, 1])
